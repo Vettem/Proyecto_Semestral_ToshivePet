b0f615a6d332c25b7d782eb3fae44f2c0d4066fc
tooopher <cr.curin@duocuc.cl>
2023-04-30 16:18:49 -0400

    Creacion jformingpro y jvalformingp (Para validar formulario de ingreso de productos )

---
+++ js/jformingpro.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $("#formingpro").submit(function(p){
+        p.preventDefault();
+        var iding = $("#idingp").val();
+        var nombreing = $("#nombreingp").val();
+        var stocking = $("#stockingp").val();
+        var fotop = $("#fotoingp").val();
+        var descping = $("#descpingp").val();
+        var precioing = $("#precioingp").val();
+        
+        var msj = "";
+        let enviar = false;
+        
+        if(iding.trim().length < 4 || iding.trim().length >4){
+            msj +="El ID debe tener 4 digitos";
+            enviar = true; 
+        }
+        iding = parseInt(iding);
+        if(!iding){
+            msj += "<br>El ID debe ser un número"
+            enviar = true;
+        }
+        var letra = nombreing.charAt(0);
+        if(!esMayuscula(letra)){
+            msj +="<br>El nombre del producto debe comenzar con una letra mayúscula";
+            enviar = true;
+        }
+        if(nombreing.trim().length < 4 || nombreing.trim().length >50){
+            msj +="<br>El nombre del producto debe tener entre 4 y 50 caracteres";
+            enviar = true;
+        }
+        if(stocking <0){
+            msj +="<br>El Stock del producto no puede ser negativo"
+            enviar = true;
+        }
+        if (descping.length < 4 ){
+            msj +="<br>La descripcion debe contener mas de 4 caracteres"
+            enviar = true;
+        }
+        if(precioing == 0){
+            msj +="<br>El precio no puede ser 0"
+            enviar = true;
+        }
+        if(precioing <0){
+            msj +="<br>El precio no puede ser negativo"
+            enviar = true;
+        }
+        if(enviar){
+            $("#warnings").html(msj);
+        }
+        else{
+            $("#warnings").html("Guardado con exito!");
+        }
+    });
+
+    function esMayuscula(letra){
+        if(letra === letra.toUpperCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+});
---
+++ js/jvalformingp.js
@@ -0,0 +1,46 @@
+var iding = document.getElementById("idingp");
+var nombreing = document.getElementById("nombreingp");
+var stocking = document.getElementById("stockingp");
+var fotop = document.getElementById("fotoingp");
+var descping = document.getElementById("descpingp");
+var precioing = document.getElementById("precioingp");
+
+const formulariop = document.getElementById("formingpro");
+
+var mensaje = document.getElementById("warnings");
+
+formulariop.addEventListener('submit', i =>{
+    i.preventDefault();
+    let msj = "";
+    let enviar = false;
+
+    if(idingp.value.length < 4 || idingp.value.length > 4){
+            msj = msj + "El ID debe 4 digitos";
+            enviar = true;
+    }
+    var letra = nombreing.value.charAt(0);
+    console.log(letra);
+    if(!esMayuscula(letra)){
+        msj +="<br>El nombre del producto debe comenzar con una letra mayúscula";
+        enviar = true;
+    }
+    if(nombreing.value.length < 4 || nombreing.value.length >50){
+        msj +="El nombre debe del producto debe tener entre 4 y 50 caracteres";
+        enviar = true;
+    }
+    if(enviar){
+        mensaje.innerHTML = msj;
+    }
+    else{
+        mensaje.innerHTML = "Guardado";
+    }
+});
+
+function esMayuscula(letra){
+    if(letra === letra.toUpperCase()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
